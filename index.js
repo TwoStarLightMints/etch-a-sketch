f2e6bb0cb3e9a6458ebfbf7cce53835ca9796811
KC Bomonti <mondo.beyondo3@gmail.com>
2022-09-29 00:50:43 -0400

    Create formula to calculate sizing for grid squares

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const container = document.querySelector('#container');
 
+const GRID_SIZE = 192;
+const INNER_BORDER_SIZE = 1;
+
+let numSquares = 64;
+let squareSizes = (192 - (INNER_BORDER_SIZE * numSquares * 2)) / numSquares;
+
 function clearBoard () {
     container.childNodes.forEach(elem => {
         elem.style.backgroundColor = 'red';
     });
 }
 
-for (let i = 0; i < 16; i++) {
-    for (let i = 0; i < 16; i++) {
+for (let i = 0; i < numSquares; i++) {
+    for (let i = 0; i < numSquares; i++) {
         let newDiv = document.createElement('div');
         // newDiv.style.display = 'inline-block';
         newDiv.style.backgroundColor = 'red';
         newDiv.style.border = '1px solid black';
         newDiv.style.display = 'inline-block';
-        newDiv.style.height = '10px';
-        newDiv.style.width = '10px';
+        newDiv.style.height = `${squareSizes}px`;
+        newDiv.style.width = `${squareSizes}px`;
         newDiv.addEventListener('mouseover', e => {
             e.target.style.backgroundColor = 'black';
         });
