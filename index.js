c9ffeadee9ca3b77754f85506d2dbe7f54a27bd2
KC Bomonti <mondo.beyondo3@gmail.com>
2022-09-29 01:18:14 -0400

    Create getNumSquares for getting user input

--- index.js
+++ index.js
@@ -3,14 +3,39 @@ const container = document.querySelector('#container');
 const GRID_SIZE = 192;
 const INNER_BORDER_SIZE = 1;
 
-let numSquares = 64;
-let squareSizes = (192 - (INNER_BORDER_SIZE * numSquares * 2)) / numSquares;
+let numSquares = 16;
 
 function clearBoard () {
     container.childNodes.forEach(elem => {
         elem.style.backgroundColor = 'red';
     });
 }
+
+function getNumSquares () {
+    let numString = prompt("Please enter the number of squares that you would like to have in the grid (please enter a whole number and for your computer's sake, keep it below 100)");
+    let clean = false;
+    let num = 0;
+
+    do {
+        num = parseInt(numString);
+        if (numString.includes(".")) {
+            numString = prompt("Please enter a whole number");
+        } else if (isNaN(num)) {
+            console.log("entered");
+            numString = prompt("Please enter a whole number");
+        } else {
+            clean = true;
+        }
+    } while (!clean);
+
+    return num;
+}
+
+function calculateSquareSizes (numSquares) {
+    return (192 - (INNER_BORDER_SIZE * numSquares * 2)) / numSquares;
+}
+
+let squareSizes = calculateSquareSizes(numSquares);
 
 for (let i = 0; i < numSquares; i++) {
     for (let i = 0; i < numSquares; i++) {
@@ -27,3 +52,5 @@ for (let i = 0; i < numSquares; i++) {
         container.appendChild(newDiv);
     }
 }
+
+console.log(getNumSquares());
